6a85e150fedba64369b01c2e728d0856d3c50e6d
unknown <bendayo38@gmail.com>
2024-01-29 16:46:28 +0100

    accessing static files - web UI

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+const exp = require('constants');
 const express = require('express')
+const path = require('path')
 
 const app = express();
+app.use(express.static('./public'));//to display static files
 
 app.get('/', (req,res)=>{
-    res.send("Hello")
+    //Alternative 2
+    //res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))//path method
 })
 
 
