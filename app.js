5d2513a594c4f99b00c0b89c6ae8a9922addd66f
ben-jy <baremu@ip-192-168-0-173.us-west-2.compute.internal>
2024-01-30 12:35:59 +0100

    sending from data js file

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const express = require('express')
 const {products} = require('./data')
 
 const app = express();
-//JSON BASICS
+
+//route parameters from home page
 app.get('/', (req,res)=>{
-  res.json(products)//array of objects -- produxts from data.js
+  res.send('<h1>Home</h1><a href="/api/products">Go to products page</a>')
    })
 
-
+//product page with limited data
+app.get('/api/products', (req,res)=>{
+    //iterate over the products
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product;
+        return {id, name, image}
+    })//ends map function
+    res.json(newProducts)
+})
 
 app.listen(3000,()=>{console.log("Thisa app is listening on port 5000")})
