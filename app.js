b1a320d8185e3da2623ab3f0df0006e815fa426f
ben-jy <baremu@ip-192-168-0-173.us-west-2.compute.internal>
2024-02-15 14:02:47 +0100

    route query

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.get('/api/products', (req,res)=>{
     res.json(newProducts)
 })
 
+//req.params
 app.get('/api/products/:productID',(req,res)=>{
     const {productID} = req.params;
     const singleProduct = products.find(
@@ -30,4 +31,23 @@ app.get('/api/products/:productID',(req,res)=>{
 
         return res.json(singleProduct)
 })
-app.listen(3000,()=>{console.log("This app is listening on port 5000")})
+
+//query string - req.query
+//http://localhost:3000/api/v1/query?name=john&gender=male  for test
+//?search=a&limit=2
+app.get('/api/v1/query',(req,res)=>{
+    //res.send('Hello')
+    //console.log(req.query)
+    const {search,limit} = req.query
+    let sortedProducts = [...products];//copied the array products
+    if(search){
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    res.status(200).json(sortedProducts)
+})
+app.listen(3000,()=>{console.log("This app is listening on port 3000")})
