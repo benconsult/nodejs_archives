ee9eadfaa10b99106621c9b9fb9b67e9b6b95e39
ben-jy <baremu@ip-192-168-0-183.us-west-2.compute.internal>
2024-02-16 12:36:14 +0100

    logger middleware with app.use function

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const app = express();
 const logger = require('./logger')
 
 //midddleware : req - middle ware - res
-//To log some stuffs on the sending a response
+//middleware simplified with app.use()
+app.use(logger)
 
-app.get('/', logger,(req,res)=>{
+app.get('/',(req,res)=>{
    
     res.send('Home')
 })
-app.get('/about',logger,(req,res)=>{
+app.get('/about',(req,res)=>{
     res.send('About')
+})
+app.get('/contact',(req,res)=>{
+    res.send('Contact Us')
 })
 
 app.listen(3000,()=>{console.log("This app is listening on port 3000")})
