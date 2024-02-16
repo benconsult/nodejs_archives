a01529d35ae95d96df89db5b5209c4759bc1d9fb
ben-jy <baremu@ip-192-168-0-183.us-west-2.compute.internal>
2024-02-16 11:34:15 +0100

    without middleware

--- app.js
+++ app.js
@@ -1,53 +1,17 @@
 const exp = require('constants');
 const express = require('express')
-const {products} = require('./data')
-
 const app = express();
 
-//route parameters from home page
-app.get('/', (req,res)=>{
-  res.send('<h1>Home</h1><a href="/api/products">Go to products page</a>')
-   })
-
-//product page with limited data
-app.get('/api/products', (req,res)=>{
-    //iterate over the products
-    const newProducts = products.map((product)=>{
-        const {id,name,image} = product;
-        return {id, name, image}
-    })//ends map function
-    res.json(newProducts)
+//midddleware : req - middle ware - res
+app.get('/',(req,res)=>{
+    const method = req.method
+    const url = req.url
+    const time  = new Date().getFullYear()
+    console.log(method,url,time);
+    res.send('Home')
+})
+app.get('/about',(req,res)=>{
+    res.send('About')
 })
 
-//req.params
-app.get('/api/products/:productID',(req,res)=>{
-    const {productID} = req.params;
-    const singleProduct = products.find(
-        (product)=> product.id === Number(productID)
-        )
-    if(!singleProduct){
-        return res.status(404).send("Page does not exist")
-      }
-
-        return res.json(singleProduct)
-})
-//http://localhost:3000/api/v1/query?search=a&limit=2
-app.get('/api/v1/query',(req,res)=>{
-    const {search,limit} = req.query
-    let sortedProducts = [...products];//copied the array products
-    if(search){
-        sortedProducts = sortedProducts.filter((product)=>{
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    if(sortedProducts.length < 1){
-       // res.status(200).send("No products matched your search")
-       //Alternatively 
-       return res.status(200).json({success: true, data: []})
-    }
-    res.status(200).json(sortedProducts)
-})
 app.listen(3000,()=>{console.log("This app is listening on port 3000")})
