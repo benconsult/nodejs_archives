0e3dd9ec2b8fbd80ef4e4034a18a5406d35d2f70
benconsult <bendayo38@gmail.com>
2024-02-19 07:01:19 +0100

    express controller router route alternative

--- routes/people.js
+++ routes/people.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const {getPeople, createPerson, createPersonPostman, updatePerson,deletePerson
 } = require('../controllers/people')
 
+/*
 router.get('/', getPeople )
 
 
@@ -19,5 +20,14 @@ router.put('/:id', updatePerson)
 
 //delete
 router.delete('/:id', deletePerson)
+*/
+
+//altrnaitve 2 to router
+//chain based on path or base url
+router.route('/').get(getPeople).post(createPerson);
+router.route('/postman').post(createPersonPostman);
+router.route('/:id').put(updatePerson).delete(deletePerson);
+ 
+
 
 module.exports = router
