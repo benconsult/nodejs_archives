b494cd34423393f7bcc1f985b6f164e947e74ec4
benconsult <bendayo38@gmail.com>
2024-02-19 02:46:07 +0100

    parsing form data setup

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ let {people} = require ('./data')
 //using static asset
 app.use(express.static('./methods-public'))
 
+//middleware for form -- parse form data
+app.use(express.urlencoded({extended:false}))
+
 app.get('/api/people', (req, res)=>{
     res.status(200).json({success:true, data:people})
     console.log(req)
 })
 
+//for form logic
+app.post('/login',(req,res)=>{
+    console.log(req.body)
+    res.send('POST')
+})
 
 app.listen(3000,()=>{console.log("This app is listening on port 3000")})
